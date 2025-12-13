3d9cd2452f621d00c12ee58d82d6ad1b474b0c01
FStevenson91 <felstefer28791@gmail.com>
2025-12-12 22:55:45 -0300

    se agrego modelo AgentResponse(pyndatic) con campos message y should_escalate, formato deterministico con output_schema. El  generate_content_config + dict falla adk no lo permite

--- real_estate_agent/agent.py
+++ real_estate_agent/agent.py
@@ -2,7 +2,11 @@ from google.adk.agents import Agent
 from google.genai import types
 from .tools import create_contact, get_contact, update_contact, list_contacts
 from .callbacks import before_model_callback
+from pydantic import BaseModel
 
+class AgentResponse(BaseModel):
+    message: str        
+    should_escalate: bool
 
 real_estate_agent = Agent(
     name="real_estate_agent",
@@ -10,6 +14,7 @@ real_estate_agent = Agent(
     description="real estate agent that qualifies leads using the BANT criteria in a few steps.",
     instruction="",
     tools =[create_contact, get_contact, update_contact, list_contacts],
-    before_model_callback=before_model_callback
-    #  generate_content_config=types.GenerateContentConfig(temperature=0.5)
+    before_model_callback=before_model_callback,
+    output_schema=AgentResponse,
+    generate_content_config=types.GenerateContentConfig(temperature=0.7)
 )
